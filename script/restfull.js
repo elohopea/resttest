a016cd897b98058be9c67b0ffce1edacf408df75
elohopea <tommi.jaske@elisanet.fi>
2018-01-30 09:25:47 +0200

    Update restfull.js

--- script/restfull.js
+++ script/restfull.js
@@ -50,6 +50,31 @@ function get_json_using_ajax(url_to_use) {
     });
 }
 
+function send_data_using_ajax(data_out, url_to_use) {
+
+    var csrftoken = getCookie('csrftoken');
+    $.ajax({
+        method: "POST",
+        contentType: 'application/json',
+        "X-CSRFToken": csrftoken,
+        xhrFields: {
+            withCredentials: false
+        },
+        crossDomain:true,
+        url: url_to_use,
+        data: data_out,
+        success: function( response ) {
+            clearResult();
+            resultTo("Result: " + JSON.stringify(response) );
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            resultTo("Status: " + textStatus); alert("Error: " + errorThrown);
+        }
+
+    });
+}
+
+
 function resultTo(response) {
     $("#result").html( response );
 }
@@ -119,7 +144,6 @@ $(document).ready( function() {
 
     // This gets the sales statistics for a game or all games as JSON from server and inserts it into the index.html
     $("#sales_stats").click( function () {
-
         var game_id = $("#game_id").val()
         var url_to_use = base_url + 'sales_stats/'
         if (game_id) {
@@ -131,36 +155,42 @@ $(document).ready( function() {
 
     // This tries to delete a game. Response from server as JSON and inserts it into the index.html
     $("#delete_game").click( function () {
-        $("#result").html("");
         var game_id = $("#game_id").val()
         if (game_id) {
             var url_to_use = base_url + 'delete_game/'
-            url_to_use = url_to_use + game_id
-            get_json_using_ajax(url_to_use);
+            url_to_use = url_to_use + game_id + "/"
+            send_data_using_ajax({}, url_to_use);
         } else {
             $("#result").html("no game id given");
         }
     });
 
+    function get_game_vars(){
+        var data = {};
+        var pk = $("#game_pk").val();
+        if (!pk) {
+            pk = 0;
+        }
+        data.pk = pk;
+        data.game_url = $("#game_url").val();
+        data.game_title = $("#game_title").val();
+        data.game_description = $("#game_description").val();
+        data.game_price = $("#game_price").val();
+        data.game_available = $("#game_available").val();
+        data.game_pic_url = $("#game_pic_url").val();
+        return data;
+    }
 
-/*
-    These are still missing
+    $("#add_game").click( function () {
+        var game_data = get_game_vars();
+        var url_to_use = base_url + 'add_game/'
+        send_data_using_ajax(game_data, url_to_use)
+    });
 
-            url
-            title
-            description
-            price
-            available
-            developer
-            created
-            updated
-            pic_url
-
-
-    path('add_game/',                views.add_game),
-    path('delete_game/<game_id>/',   views.delete_game),
-    path('modify_game/<game_id>/',   views.modify_game),
-*/
-
+    $("#modify_game").click( function () {
+        var game_data = get_game_vars();
+        var url_to_use = base_url + 'modify_game/'
+        send_data_using_ajax(game_data, url_to_use)
+    });
 
 });
